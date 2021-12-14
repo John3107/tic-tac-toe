1fed5da36e1946c5e999338796a5fbd0cf7b2ee0
John3107 <maynsayt@gmail.com>
2021-12-14 17:53:36 +0200

    Init commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import Game from "./pages/Game";
+import useTickTackToe from "./hooks/useTickTackToe";
+import Start from "./pages/Start";
+import Finished from "./pages/Finished";
+import * as React from "react";
+const App = () => {
+    const game = useTickTackToe();
+    return (
+        <div className="App">
+            {game.status === "created" && <Start handleStart={game.handleStart} />}
+            {game.status === "finished" && <Finished name={game.name} handleRestart={game.handleRestart} />}
+            {game.status === "started" && <Game board={game.board} handleClick={game.handleClick} />}
+        </div>
+    );
+};
 export default App;
---
+++ src/pages/Finished.tsx
@@ -0,0 +1,18 @@
+
+type FinishedPropsType = {
+    name: string | null | undefined;
+    handleRestart?: () => void;
+}
+const Finished = (props: FinishedPropsType) => {
+    const { name, handleRestart } = props;
+    return (
+        <div>
+            <h1>
+                {name && `Player ${name} won the game`}
+                {!name && "It's a tie "}
+            </h1>
+            <button onClick={handleRestart}>Restart</button>
+        </div>
+    );
+};
+export default Finished;
